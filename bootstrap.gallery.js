763a6036821dd3b128b9322a5cffa1155aeebff0
Raúl Sepúlveda <raulsetron@gmail.com>
2015-04-17 09:37:31 -0300

    para bootstrap3

--- bootstrap.gallery.js
+++ bootstrap.gallery.js
@@ -1,26 +1,25 @@
 !function ($) {
   // constructor
   var Gallery = function (element, options) {
-    this.$element = element
+    this.$element = $(element)
     $carousel = $('.carousel', element)
     this.options = options
 
     $carousel.carousel(options)
 
     var $items = $('.carousel .item', this.$element)
-    $carousel.on('slid', function(e) {
+    $carousel.on('slid.bs.carousel', function(e) {
       var index = $('.item.active', this.$element).index('.item', this.$element)
-      $('.thumbnail', element).removeClass('active')
-      $('.thumbnail[data-item=' + index + ']', element).addClass('active')
+      $(options.itemSelector, element).removeClass('active')
+      $(options.itemSelector+'[data-item=' + index + ']', element).addClass('active')
     })
-
-    $('.thumbnail', element).live('click', function(e) {
+    this.$element.on('click', options.itemSelector, function(e) {
       var target = $(this).data('item')
       $carousel.carousel(parseInt(target))
       e.preventDefault()
     })
 
-    $('.carousel-control', element).live('click', function(e) {
+    this.$element.on('click', '.carousel-control', function(e) {
       var to = $(this).data('slide')
       $carousel.carousel(to)
       e.preventDefault()
@@ -39,7 +38,7 @@
       
       if (!data) {
         $this.data('gallery', (data = new Gallery(this, options)))
-        $('.thumbnail', $this).each(function(i, element) {
+        $(options.itemSelector, $this).each(function(i, element) {
           $(element).attr('data-item', i)
         })
       }
@@ -47,6 +46,7 @@
   }
 
   $.fn.gallery.defaults = {
+    itemSelector: '.thumbnail',
   }
 
 }(window.jQuery)
